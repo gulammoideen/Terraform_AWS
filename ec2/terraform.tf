888283b8f1006b87ee258d95c37376fbc43e8cd1
cognis-sudhansu <sudhansu@cognissolutions.com>
2024-03-21 14:45:46 +0530

    vcs_ec2_1

---
+++ ec2/terraform.tf
@@ -0,0 +1,19 @@
+# terraform {
+
+#   cloud {
+#     organization = "gulammoideen"
+
+#     workspaces {
+#       name = "Day1_exercise"
+#     }
+#   }
+
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.41.0"
+#     }
+#   }
+
+#   required_version = ">= 1.7.0"
+# }
