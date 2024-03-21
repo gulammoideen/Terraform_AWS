ce85df8096fc6a8026b40bc0b7769fd202983bca
cognis-sudhansu <sudhansu@cognissolutions.com>
2024-03-21 14:51:55 +0530

    vcs_ec2_2

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 resource "aws_instance" "terraformtest" {
     ami = "ami-0d7a109bf30624c99"
-    instance_type = "t2.micro"
+    instance_type = "t2.small"
     key_name = "devops"
 }
